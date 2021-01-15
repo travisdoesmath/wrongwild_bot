025795b3fa5b5d13d6723b818cf32575703c419d
Travis Taylor <travisdoesmath@gmail.com>
2021-01-15 11:17:35 -0600

    added handling for suspended users

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ reddit = praw.Reddit(
 subreddit = reddit.subreddit("gonwild")
 
 def authors_nsfw_percentage(author):
+    if hasattr(author, 'is_suspended'):
+            return 100
     nsfw_count = 0
     sfw_count = 0
     for submission in author.submissions.new():
@@ -26,5 +28,5 @@ for submission in subreddit.stream.submissions():
     nsfw_percentage = authors_nsfw_percentage(author)
     # print(author, nsfw_percentage)
     if nsfw_percentage > 0.5:
-        submission.report("User is likely a porn bot. Over half of their submissions are to NSFW subreddits.")
+        submission.report("User is likely a porn bot. Over half of their submissions are to NSFW subreddits or their account is suspended.")
         submission.reply("This post has been reported to the moderators and /u/SabreYT has been tagged.\n\nMore than half of the user's submissions are to (non-gonwild) NSFW subreddits\n\n---\n\n^(I am a bot created by /u/travisdoesmath)")
